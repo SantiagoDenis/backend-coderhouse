56903b838499105f42b08de116d49e430ffa7547
Santiago Denis <santidenis.sd@gmail.com>
2022-06-30 17:51:36 -0300

    Challenge class 20

---
+++ src/daos/products/productsFiles.js
@@ -0,0 +1,10 @@
+import { FilesContainer } from "../../containers/filesContainer.js";
+
+class DaoProductsFiles extends FilesContainer {
+    constructor() {
+        super('products.txt')
+    }
+}
+
+export default DaoProductsFiles
+
