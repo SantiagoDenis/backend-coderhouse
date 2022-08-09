bc72813c471aaf837e34e81fba0f5052ce288942
Santiago Denis <santidenis.sd@gmail.com>
2022-08-09 08:45:22 -0300

    Challenge class 30

--- src/index.js
+++ src/index.js
@@ -193,7 +193,7 @@ app.get('/info', (req, res) => {
     res.send({'Arguments': process.argv, 'Platform name': process.platform, 'Node version': process.version, 'Memoria reservada': process.memoryUsage, 'path': process.cwd(), 'id': process.pid})
 })
 
-
+//Fork process
 app.get('/random', (req, res) => {
     const iterations = req.query.number || 20000
     console.log('random iterations', `${iterations}`)
@@ -212,11 +212,21 @@ app.use('/', authRouter)
 let args = minimist(process.argv, {alias: {'p': 'port'}})
 const port = process.env.PORT
 //Listener for the server
-const server = app.listen(args.port || port, () => console.log(`Server active at port: ${server.address().port}`))
+const PORT = args.port
 
-//Error handler for the server listener
-server.on('error', (error) => console.error(`Error on listening to server: ${error}`));
+if (args.modo == 'CLUSTER') {
+    if (cluster.isPrimary) {
+        for (let i = 0; i < numCPU; i++) {
+            cluster.fork()
+        }
+    } else {
+        httpServer.listen(PORT, () => {});
+        httpServer.on("error", (error) => console.error(error, "error de conexión"));
+    }
+}
+if (args.modo == 'FORK'){
+httpServer.listen(PORT, () => {
+    console.log(`Escuchando en el puerto ${httpServer.address().port}`);
+})
+}
 
-/* 
-[{"title":"Backpack","price":250,"thumbnail":"https://cdn3.iconfinder.com/data/icons/education-209/64/bag-pack-container-school-256.png","stock":100,"description":"A bag put on somebody's back. It has two straps that go over the shoulders. It is used to carry things in it, and it often has many pockets or compartments to carry things.","timestamp":"04/05/2022 14:50:05","id":1},{"title":"Ruler","price":40,"thumbnail":"https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png","stock":100,"description":"A tool or device used to measure length and draw straight lines. A ruler is used to measure the length in both metric and customary units. The rulers are marked with standard distance in centimeters in the top and inches in the bottom and the intervals in the ruler are called hash marks.","timestamp":"04/05/2022 14:55:22","id":2},{"title":"Pencil","price":20,"thumbnail":"https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png","stock":100,"description":"A writing or drawing implement with a solid pigment core encased in a sleeve, barrel, or shaft that prevents breaking the core or marking a user's hand.","timestamp":"04/05/2022 14:55:22","id":3},{"title":"Note pad","price":70,"thumbnail":"https://cdn3.iconfinder.com/data/icons/education-209/64/paper-clip-academic-note-exam-256.png","stock":100,"description":"A book or stack of paper pages that are often ruled and used for purposes such as recording notes or memoranda, other writing, drawing or scrapbooking.","timestamp":"04/05/2022 14:59:09","id":4},{"title":"Calculator","price":100,"thumbnail":"https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png","stock":100,"description":"A device that performs arithmetic operations on numbers. It can do from addition, subtraction, multiplication, and division to more sophisticated operations such as can handle exponential operations, roots, logarithms, etc.","timestamp":"04/05/2022 14:59:09","id":5},{"title":"Board","price":300,"thumbnail":"https://cdn3.iconfinder.com/data/icons/education-209/64/board-math-class-school-256.png","stock":100,"description":"Is a reusable writing surface on which texts and figures are drawn with chalk or other types of erasable markers. ","timestamp":"04/05/2022 14:59:09","id":6}]
-*/
